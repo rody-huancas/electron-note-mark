1a4b5a43dc71153f6f4fa46a6d9fc1da2a6790a5
Rody Huancas <rodyhuancas.04@gmail.com>
2024-04-01 20:06:11 -0500

    Add: logo header

--- src/renderer/src/components/DraggableTopBar.tsx
+++ src/renderer/src/components/DraggableTopBar.tsx
@@ -1,5 +1,11 @@
 export const DraggableTopBar = () => {
   return (
-    <header className="absolute inset-0 h-8 bg-transparent"/>
-  )
-}
+    <header className="absolute inset-0 h-8 bg-transparent">
+      <nav className="w-[250px] h-full flex items-center justify-center bg-zinc-900/75 text-gray-100 p-2 text-lg">
+        <h1 className="font-extrabold">
+          Note<span className="text-indigo-400">Mark</span>
+        </h1>
+      </nav>
+    </header>
+  );
+};
